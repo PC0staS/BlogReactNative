9ce23a37cdf97292a9c98bae5d36a19992570ba8
PC0staS <pablocostasnieto@gmail.com>
2025-08-31 01:10:10 +0200

    Update BACKEND_URL to correct local server address

--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,3 +1,3 @@
 import Constants from 'expo-constants';
 
-export const BACKEND_URL = Constants.expoConfig?.extra?.BACKEND_URL || process.env.BACKEND_URL || 'http://192.168.1.86:3001';
+export const BACKEND_URL = Constants.expoConfig?.extra?.BACKEND_URL || process.env.BACKEND_URL || 'http://192.168.1.57:3001';
